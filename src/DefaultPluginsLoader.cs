8a3124a7930a061b59c507bdf4b086d113301445
Mykhailo.Romaniuk <mishani0x0ef@gmail.com>
2017-06-27 00:48:02 +0300

    Implement factory for resolve default plugins loader.

---
+++ src/DefaultPluginsLoader.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace PluginsLoader
+{
+    /// <summary>
+    /// Default implementation of plugins loader.
+    /// </summary>
+    public class PluginsLoader : IPluginsLoader
+    {
+        private const string PluginsFileNamePattern = "*.dll";
+
+        public IEnumerable<T> LoadFrom<T>(string path, bool includeSubfolders = false) where T : class
+        {
+            if (Directory.Exists(path))
+            {
+                return LoadPluginsFromDirectory<T>(path, includeSubfolders);
+            }
+            else if (File.Exists(path))
+            {
+                return LoadPluginsFromFile<T>(path);
+            }
+            else
+            {
+                throw new DirectoryNotFoundException();
+            }
+        }
+
+        private IEnumerable<T> LoadPluginsFromDirectory<T>(string dirPath, bool includeSubfolders) where T : class
+        {
+            var searchOptions = includeSubfolders
+                   ? SearchOption.AllDirectories
+                   : SearchOption.TopDirectoryOnly;
+
+            var files = Directory.GetFiles(dirPath, PluginsFileNamePattern, searchOptions);
+
+            foreach (var file in files)
+            {
+                var plugins = LoadPluginsFromFile<T>(file);
+                foreach (var plugin in plugins)
+                {
+                    yield return plugin;
+                }
+            }
+        }
+
+        private IEnumerable<T> LoadPluginsFromFile<T>(string filePath) where T: class
+        {
+            var assemblyName = AssemblyName.GetAssemblyName(filePath);
+            var assembly = Assembly.Load(assemblyName);
+
+            if (assembly == null)
+            {
+                yield break;
+            }
+
+            var targetType = typeof(T);
+
+            var assemblyTypes = assembly.GetTypes();
+            foreach (var type in assemblyTypes)
+            {
+                if (type.IsAbstract || type.IsInterface || type.GetInterface(targetType.Name) == null)
+                {
+                    continue;
+                }
+
+                if (Activator.CreateInstance(type) is T instance)
+                {
+                    yield return instance;
+                }
+            }
+        }
+    }
+}
--- src/PluginsLoader.cs
+++ src/PluginsLoader.cs
@@ -1,74 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-
-namespace PluginsLoader
+﻿namespace PluginsLoader
 {
     /// <summary>
-    /// Default implementation of plugins loader.
+    /// Provider of plugins loaders.
     /// </summary>
-    public class PluginsLoader : IPluginsLoader
+    public static class PluginsLoader
     {
-        private const string PluginsFileNamePattern = "*.dll";
+        private static IPluginsLoader _defaultLoader;
 
-        public IEnumerable<T> LoadFrom<T>(string path, bool includeSubfolders = false) where T : class
+        private static object _locker;
+
+        /// <summary>
+        /// Default and recommended implementation of pugins loader.
+        /// </summary>
+        public static IPluginsLoader Default
         {
-            if (Directory.Exists(path))
+            get
             {
-                return LoadPluginsFromDirectory<T>(path, includeSubfolders);
-            }
-            else if (File.Exists(path))
-            {
-                return LoadPluginsFromFile<T>(path);
-            }
-            else
-            {
-                throw new DirectoryNotFoundException();
-            }
-        }
+                if (_defaultLoader != null)
+                    return _defaultLoader;
 
-        private IEnumerable<T> LoadPluginsFromDirectory<T>(string dirPath, bool includeSubfolders) where T : class
-        {
-            var searchOptions = includeSubfolders
-                   ? SearchOption.AllDirectories
-                   : SearchOption.TopDirectoryOnly;
-
-            var files = Directory.GetFiles(dirPath, PluginsFileNamePattern, searchOptions);
-
-            foreach (var file in files)
-            {
-                var plugins = LoadPluginsFromFile<T>(file);
-                foreach (var plugin in plugins)
+                lock (_locker)
                 {
-                    yield return plugin;
-                }
-            }
-        }
-
-        private IEnumerable<T> LoadPluginsFromFile<T>(string filePath) where T: class
-        {
-            var assemblyName = AssemblyName.GetAssemblyName(filePath);
-            var assembly = Assembly.Load(assemblyName);
-
-            if (assembly == null)
-            {
-                yield break;
-            }
-
-            var targetType = typeof(T);
-
-            var assemblyTypes = assembly.GetTypes();
-            foreach (var type in assemblyTypes)
-            {
-                if (type.IsAbstract || type.IsInterface || type.GetInterface(targetType.Name) == null)
-                {
-                    continue;
-                }
-
-                if (Activator.CreateInstance(type) is T instance)
-                {
-                    yield return instance;
+                    return _defaultLoader ?? (_defaultLoader = new DefaultPluginsLoader());
                 }
             }
         }
